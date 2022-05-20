c9ad9b21d83adc92ff597660a676eceba45670f6
Sitesauce <action@sitesauce.app>
2022-05-20 12:45:16 +0000

    Update static site

---
+++ wp-login.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+	<html lang="en-US">
+	<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<title>Log In &lsaquo; 2022 카지노사이트 와 바카라사이트 모음 &#8212; WordPress</title>
+	<meta name='robots' content='noindex, follow' />
+<link rel='dns-prefetch' href='//s.w.org' />
+<link rel='stylesheet' id='dashicons-css'  href='https://projectfluent.io/wp-includes/css/dashicons.min.css?ver=5.9.3' type='text/css' media='all' />
+<link rel='stylesheet' id='buttons-css'  href='https://projectfluent.io/wp-includes/css/buttons.min.css?ver=5.9.3' type='text/css' media='all' />
+<link rel='stylesheet' id='forms-css'  href='https://projectfluent.io/wp-admin/css/forms.min.css?ver=5.9.3' type='text/css' media='all' />
+<link rel='stylesheet' id='l10n-css'  href='https://projectfluent.io/wp-admin/css/l10n.min.css?ver=5.9.3' type='text/css' media='all' />
+<link rel='stylesheet' id='login-css'  href='https://projectfluent.io/wp-admin/css/login.min.css?ver=5.9.3' type='text/css' media='all' />
+	<meta name='referrer' content='strict-origin-when-cross-origin' />
+		<meta name="viewport" content="width=device-width" />
+	<link rel="icon" href="https://projectfluent.io/wp-content/uploads/2022/03/cropped-939-9398801_tw-website-icon-globe-in-green-icon-32x32.jpg" sizes="32x32" />
+<link rel="icon" href="https://projectfluent.io/wp-content/uploads/2022/03/cropped-939-9398801_tw-website-icon-globe-in-green-icon-192x192.jpg" sizes="192x192" />
+<link rel="apple-touch-icon" href="https://projectfluent.io/wp-content/uploads/2022/03/cropped-939-9398801_tw-website-icon-globe-in-green-icon-180x180.jpg" />
+<meta name="msapplication-TileImage" content="https://projectfluent.io/wp-content/uploads/2022/03/cropped-939-9398801_tw-website-icon-globe-in-green-icon-270x270.jpg" />
+	</head>
+	<body class="login no-js login-action-login wp-core-ui  locale-en-us">
+	<script type="text/javascript">
+		document.body.className = document.body.className.replace('no-js','js');
+	</script>
+		<div id="login">
+		<h1><a href="https://wordpress.org/">Powered by WordPress</a></h1>
+	
+		<form name="loginform" id="loginform" action="https://projectfluent.io/wp-login.php" method="post">
+			<p>
+				<label for="user_login">Username or Email Address</label>
+				<input type="text" name="log" id="user_login" class="input" value="" size="20" autocapitalize="off" />
+			</p>
+
+			<div class="user-pass-wrap">
+				<label for="user_pass">Password</label>
+				<div class="wp-pwd">
+					<input type="password" name="pwd" id="user_pass" class="input password-input" value="" size="20" />
+					<button type="button" class="button button-secondary wp-hide-pw hide-if-no-js" data-toggle="0" aria-label="Show password">
+						<span class="dashicons dashicons-visibility" aria-hidden="true"></span>
+					</button>
+				</div>
+			</div>
+						<p class="forgetmenot"><input name="rememberme" type="checkbox" id="rememberme" value="forever"  /> <label for="rememberme">Remember Me</label></p>
+			<p class="submit">
+				<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="Log In" />
+									<input type="hidden" name="redirect_to" value="https://projectfluent.io/wp-admin/" />
+									<input type="hidden" name="testcookie" value="1" />
+			</p>
+		</form>
+
+					<p id="nav">
+								<a href="https://projectfluent.io/wp-login.php?action=lostpassword">Lost your password?</a>
+			</p>
+					<script type="text/javascript">
+			function wp_attempt_focus() {setTimeout( function() {try {d = document.getElementById( "user_login" );d.focus(); d.select();} catch( er ) {}}, 200);}
+wp_attempt_focus();
+if ( typeof wpOnload === 'function' ) { wpOnload() }		</script>
+				<p id="backtoblog">
+			<a href="https://projectfluent.io/">&larr; Go to 2022 카지노사이트 와 바카라사이트 모음</a>		</p>
+			</div>
+			<script type='text/javascript' src='https://projectfluent.io/wp-includes/js/jquery/jquery.min.js?ver=3.6.0' id='jquery-core-js'></script>
+<script type='text/javascript' src='https://projectfluent.io/wp-includes/js/jquery/jquery-migrate.min.js?ver=3.3.2' id='jquery-migrate-js'></script>
+<script type='text/javascript' id='zxcvbn-async-js-extra'>
+/* <![CDATA[ */
+var _zxcvbnSettings = {"src":"https:\/\/projectfluent.io\/wp-includes\/js\/zxcvbn.min.js"};
+/* ]]> */
+</script>
+<script type='text/javascript' src='https://projectfluent.io/wp-includes/js/zxcvbn-async.min.js?ver=1.0' id='zxcvbn-async-js'></script>
+<script type='text/javascript' src='https://projectfluent.io/wp-includes/js/dist/vendor/regenerator-runtime.min.js?ver=0.13.9' id='regenerator-runtime-js'></script>
+<script type='text/javascript' src='https://projectfluent.io/wp-includes/js/dist/vendor/wp-polyfill.min.js?ver=3.15.0' id='wp-polyfill-js'></script>
+<script type='text/javascript' src='https://projectfluent.io/wp-includes/js/dist/hooks.min.js?ver=1e58c8c5a32b2e97491080c5b10dc71c' id='wp-hooks-js'></script>
+<script type='text/javascript' src='https://projectfluent.io/wp-includes/js/dist/i18n.min.js?ver=30fcecb428a0e8383d3776bcdd3a7834' id='wp-i18n-js'></script>
+<script type='text/javascript' id='wp-i18n-js-after'>
+wp.i18n.setLocaleData( { 'text direction\u0004ltr': [ 'ltr' ] } );
+</script>
+<script type='text/javascript' id='password-strength-meter-js-extra'>
+/* <![CDATA[ */
+var pwsL10n = {"unknown":"Password strength unknown","short":"Very weak","bad":"Weak","good":"Medium","strong":"Strong","mismatch":"Mismatch"};
+/* ]]> */
+</script>
+<script type='text/javascript' id='password-strength-meter-js-translations'>
+( function( domain, translations ) {
+	var localeData = translations.locale_data[ domain ] || translations.locale_data.messages;
+	localeData[""].domain = domain;
+	wp.i18n.setLocaleData( localeData, domain );
+} )( "default", { "locale_data": { "messages": { "": {} } } } );
+</script>
+<script type='text/javascript' src='https://projectfluent.io/wp-admin/js/password-strength-meter.min.js?ver=5.9.3' id='password-strength-meter-js'></script>
+<script type='text/javascript' src='https://projectfluent.io/wp-includes/js/underscore.min.js?ver=1.13.1' id='underscore-js'></script>
+<script type='text/javascript' id='wp-util-js-extra'>
+/* <![CDATA[ */
+var _wpUtilSettings = {"ajax":{"url":"\/wp-admin\/admin-ajax.php"}};
+/* ]]> */
+</script>
+<script type='text/javascript' src='https://projectfluent.io/wp-includes/js/wp-util.min.js?ver=5.9.3' id='wp-util-js'></script>
+<script type='text/javascript' id='user-profile-js-extra'>
+/* <![CDATA[ */
+var userProfileL10n = {"user_id":"0","nonce":"799cd86931"};
+/* ]]> */
+</script>
+<script type='text/javascript' id='user-profile-js-translations'>
+( function( domain, translations ) {
+	var localeData = translations.locale_data[ domain ] || translations.locale_data.messages;
+	localeData[""].domain = domain;
+	wp.i18n.setLocaleData( localeData, domain );
+} )( "default", { "locale_data": { "messages": { "": {} } } } );
+</script>
+<script type='text/javascript' src='https://projectfluent.io/wp-admin/js/user-profile.min.js?ver=5.9.3' id='user-profile-js'></script>
+	<div class="clear"></div>
+	</body>
+	</html>
+	
